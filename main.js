874dfd6167c9f61eb6ed363bcdc1f54f806daa31
Kevin Hill <kahill2014@gmail.com>
2020-07-18 18:38:42 -0500

    Update animation easing to swing rather than linear

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(function(){
     $("#sliding-box").click(function(){
-        $("#sliding-box").animate({left:"50%"}, 2000, "linear")
-            .animate({top:"50%"}, 2000, "linear");
-        $("#sliding-box").animate({left:"0"}, 2000, "linear")
-            .animate({top:"0"}, 2000, "linear");
+        $("#sliding-box").animate({left:"50%"}, 2000, "swing")
+            .animate({top:"50%"}, 2000, "swing");
+        $("#sliding-box").animate({left:"0"}, 2000, "swing")
+            .animate({top:"0"}, 2000, "swing");
     });
 });
